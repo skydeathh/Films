7fb730e4f069e712c09c4c64e3718874885d1b69
skydeathh <a1b2f2f2@gmail.com>
2024-07-24 09:45:13 +0700

    Fixed README

--- Films/Models/PageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Films.Models
-{
-    public class HomePageModel
-    {
-        public int CustomContent { get; set; }
-        public string ProductStatus { get; set; }
-        public string DateOnly { get; set; }
-    }
-}
